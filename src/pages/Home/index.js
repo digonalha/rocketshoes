5c3766005684ef41feb906315e84fdfb94c7679a
digonalha <digonalha@gmail.com>
2020-05-29 22:12:24 -0300

    criando estilo da home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
-// import { Container } from './styles';
-
-function Home() {
-  return <div />;
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://authenticfeet.vteximg.com.br/arquivos/ids/225940-1000-1000/Tenis-adidas-Nite-Jogger-Cinza.jpg?v=636905283194000000"
+          alt="Tenis"
+        />
+        <strong>Exemplo tenis</strong>
+        <span>R$200,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://authenticfeet.vteximg.com.br/arquivos/ids/225940-1000-1000/Tenis-adidas-Nite-Jogger-Cinza.jpg?v=636905283194000000"
+          alt="Tenis"
+        />
+        <strong>Exemplo tenis</strong>
+        <span>R$200,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://authenticfeet.vteximg.com.br/arquivos/ids/225940-1000-1000/Tenis-adidas-Nite-Jogger-Cinza.jpg?v=636905283194000000"
+          alt="Tenis"
+        />
+        <strong>Exemplo tenis</strong>
+        <span>R$200,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size="16" color="#FFF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
-
-export default Home;
