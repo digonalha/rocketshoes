3a06cd601417ac9b078bbb4a9d4ca2f4f0a7018d
digonalha <digonalha@gmail.com>
2020-05-30 22:31:34 -0300

    estilizando cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
-function Cart() {
-  return <div />;
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://authenticfeet.vteximg.com.br/arquivos/ids/225940-1000-1000/Tenis-adidas-Nite-Jogger-Cinza.jpg?v=636905283194000000"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Modelo do tenis</strong>
+              <span> R$199,99</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$399,98</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$250,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
-
-export default Cart;
