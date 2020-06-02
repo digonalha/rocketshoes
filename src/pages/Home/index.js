950fda4bd6cd3e4d407e81f782ad944162111035
digonalha <digonalha@gmail.com>
2020-06-01 21:50:18 -0300

    iterando os produtos e apresentando na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,58 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://authenticfeet.vteximg.com.br/arquivos/ids/225940-1000-1000/Tenis-adidas-Nite-Jogger-Cinza.jpg?v=636905283194000000"
-          alt="Tenis"
-        />
-        <strong>Exemplo tenis</strong>
-        <span>R$200,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size="16" color="#FFF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://authenticfeet.vteximg.com.br/arquivos/ids/225940-1000-1000/Tenis-adidas-Nite-Jogger-Cinza.jpg?v=636905283194000000"
-          alt="Tenis"
-        />
-        <strong>Exemplo tenis</strong>
-        <span>R$200,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size="16" color="#FFF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://authenticfeet.vteximg.com.br/arquivos/ids/225940-1000-1000/Tenis-adidas-Nite-Jogger-Cinza.jpg?v=636905283194000000"
-          alt="Tenis"
-        />
-        <strong>Exemplo tenis</strong>
-        <span>R$200,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size="16" color="#FFF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatedPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={products.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size="16" color="#FFF" />1
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
