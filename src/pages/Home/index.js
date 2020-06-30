43e8f7694901da020cd656890c57ca2b30af03a9
Rodrigo Fernandes <digonalha@gmail.com>
2020-06-29 23:16:53 -0300

    utilizando useSelector para acessar informacao do Redux; utilizando useDispatch para disparar uma acao no redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ProductList } from './styles';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 import * as CartActions from '../../store/modules/cart/actions';
 
-function Home({ amount, addToCartRequest }) {
+export default function Home() {
   const [products, setProducts] = useState([]);
+  const amount = useSelector((state) =>
+    state.cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.amount;
+      return sumAmount;
+    }, {})
+  );
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function loadProducts() {
@@ -27,7 +34,7 @@ function Home({ amount, addToCartRequest }) {
 
   function handleAddProduct(id) {
     // dispara as acoes do redux/redux-saga
-    addToCartRequest(id);
+    dispatch(CartActions.addToCartRequest(id));
   }
 
   return (
@@ -49,16 +56,3 @@ function Home({ amount, addToCartRequest }) {
     </ProductList>
   );
 }
-
-const mapStateToProps = (state) => ({
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount;
-    return amount;
-  }, {}),
-});
-
-const mapDispacthToProps = (dispatch) =>
-  bindActionCreators(CartActions, dispatch);
-
-// vincula nossos componentes com o redux
-export default connect(mapStateToProps, mapDispacthToProps)(Home);
